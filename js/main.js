2069ed1ea455e1bfb72c72b97136d07c224f9e8f
Chris Devalme <cdevalme@gmail.com>
2023-12-02 14:57:28 -0500

    updated javasript to show player icons

--- js/main.js
+++ js/main.js
@@ -8,21 +8,24 @@ const ICONS = {
 
 /*----- state variables -----*/
 
-let turn; // Which playyer is going
-let board; // the game board
-let winner; // Winner of game
+let turn // Which playyer is going
+let board  // the game board
+let winner // Winner of game
 
 /*----- cached elements  -----*/
-const messageEl = document.getElementById('game-message');
-const restartbtn = document.getElementById('reset-btn');
-const boardSpaces = document.querySelectorAll('.board > div');
+
+const messageEl = document.getElementById('game-message')
+const restartbtn = document.getElementById('reset-btn')
+const boardSpaces = [...document.querySelectorAll('#board > div')] // ... turns node list into array
 
 /*----- event listeners -----*/
+
+document.querySelector('#board').addEventListener('click', playerMove)
 
 
 /*----- functions -----*/
 
-freshBoard();
+freshBoard()
 
 function freshBoard() { //funtion to start game
   turn = 1;
@@ -32,37 +35,64 @@ function freshBoard() { //funtion to start game
     [0, 0, 0]  //c2
     //r0 r1 r2
   ]
-  winner = null;
-  render();
+  winner = null
+  render()
+}
+
+function playerMove(event) {
+  if (event.target.classList.contains('blank')){
+    event.target.classList.remove(`blank`)
+    event.target.classList.add(`${ICONS[turn]}`)
+    event.target.innerText = `${ICONS[turn]}`
+
+    board.forEach(function(columnArray, columnIndex) { // looping over each item of array and each array its self
+      columnArray.forEach(function(rowValue, rowIndex){
+        const boardSpace = `c${columnIndex}r${rowIndex}`
+        const boardEl = document.getElementById(boardSpace)
+      })
+    })
+    
+    turn *= -1
+    console.log(event.target)
+    console.log(turn)
+  }
 }
 
 function render() { // function to tranfer state of app to the DOM
-  renderFreshBoard();
-  renderMessage();
-  renderControls();
+  renderFreshBoard()
+  renderMessage()
+  renderControls()
 }
 
 function renderFreshBoard() {
   board.forEach(function(columnArray, columnIndex) { // looping over each item of array and each array its self
     columnArray.forEach(function(rowValue, rowIndex){
-      const boardSpace = `c${columnIndex}r${rowIndex}`;
-      const boardEl = document.getElementById(boardSpace);
+      const boardSpace = `c${columnIndex}r${rowIndex}`
+      const boardEl = document.getElementById(boardSpace)
       boardEl.classList.add(`${ICONS[rowValue]}`)
       boardEl.innerText = ICONS[rowValue]
     })
-  });
+  })
 }
 
 function renderMessage() {
-  if (winner === 'Tie'){
-    messageEl.innerHTML = `TIE GAME! <br> Click The Restart Button To Play Again`;
+  if (winner === 'Tie') {
+    messageEl.innerHTML = `TIE GAME! <br> Click The Restart Button To Play Again`
   } else if (winner) {
-    messageEl.innerHTML = `<span style="color: red; font-size: 5vmin; margin-right: 3vmin;">${ICONS[turn]}</span> <span style="font-size: 5vmin;">WINS!!!</span>`;
+    messageEl.innerHTML = `<span style="color: red; font-size: 5vmin; margin-right: 3vmin;">${ICONS[turn]}</span> <span style="font-size: 5vmin;">WINS!!!</span>`
   } else {
-    messageEl.innerHTML = `<span style="color: red; font-size: 5vmin; margin-right: 3vmin;">${ICONS[turn]}</span> <span style="font-size: 5vmin;">It's Your Go!</span>`;
+    messageEl.innerHTML = `<span style="color: red; font-size: 5vmin; margin-right: 3vmin;">${ICONS[turn]}</span> <span style="font-size: 5vmin;">It's Your Go!</span>`
   }
 }
 
 function renderControls() {
   restartbtn.style.visibility = winner ? 'visible' : 'hidden'
+
+  // boardSpaces.forEach(function(columnArray, columnIndex) { // looping over each item of array and each array its self
+  //   columnArray.forEach(function(rowValue, rowIndex){
+  //     const boardSpace = `c${columnIndex}r${rowIndex}`
+  //     const boardEl = document.getElementById(boardSpace)
+  //     boardEl
+  //   })
+  // });
 }
